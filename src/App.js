7f7a4688b169551395b3fe09566b58e13030c087
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2018-09-27 21:16:55 -0300

    Add a form to create new projects
    - Create a new Component called AddProject
    - Create a form and make it works to add a new Project to our
    application.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
       projects: [
         {
           title: 'Business Website',
@@ -21,13 +28,20 @@ class App extends Component {
           category: 'Low Level Programming'
         }
       ]
-    }
+    })
+  }
+
+  handleAddProject(project) {
+    console.log(project);
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
   }
 
   render() {
     return (
       <div className="App">
-        My App
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects}/>
       </div>
     );
